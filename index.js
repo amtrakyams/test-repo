869ce19cbb088fd22ff1df02876ff5147f3646d2
Mark Matyas <quic_mmatyas@quicinc.com>
2021-04-02 15:23:16 -0700

    Add sample script
    Signed-off-by: Mark Matyas <quic_mmatyas@quicinc.com>

---
+++ index.js
@@ -0,0 +1,6 @@
+export function hi(){
+  return "Hello World";
+}
+
+console.log(hi());
+
---
+++ test.js
@@ -0,0 +1,4 @@
+import assert from 'assert/strict';
+import {hi} from './index.js';
+
+assert.equal(hi(), "Hello World");
